7047f09fe66d89666129b4e6b4b93a58987e949b
ashokba6266 <ashokba6266@gmail.com>
2020-05-08 23:33:43 +0200

    Actiavated event todo list

--- pwa-firebase/todo/pwa-tutorial-lesson-2/sw.js
+++ pwa-firebase/todo/pwa-tutorial-lesson-2/sw.js
@@ -1,2 +1,11 @@
 //install service worker
+self.addEventListener('install', evt =>{
+    console.log('service worker installed')
+});
 
+//activate service worker
+
+self.addEventListener('activate', evt =>{
+    console.log('service worker has been activated');
+
+});
